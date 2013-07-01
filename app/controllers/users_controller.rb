d62f59634b0dcdc04e6bc0f2118cee71b6a8f76d
Daichi Ueda <ueda.daichi@gmail.com>
2013-07-01 12:42:40 -0500

    Finish user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   include SessionsHelper
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -17,5 +20,38 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+def index
+    @users = User.paginate(page: params[:page])
+end
+
+private
+  def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+      #Note that this is equivalient to below:
+      #unless signed_in?
+        #flash[:notice] = "Please sign in."
+        #redirect_to signin_url
+      #end
+  end
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,10 +4,20 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin page" do
+  describe "signin" do
+
     before { visit signin_path }
 
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
   end
 end
